494455db49abb1a55a7bd15390c25c0b5daee151
fabiibet <fab.betancourt@duocuc.cl>
2023-06-06 23:21:58 -0400

    ActualizacionDjango

--- Entrega-3/apps/petSociety/urls.py
+++ Entrega-3/apps/petSociety/urls.py
@@ -4,3 +4,16 @@ from . import views
 urlpatterns = [
     path('',views.cargarInicio)
 ]
+
+urlpatterns = [
+    path('',views.cargarPerros)
+]
+
+urlpatterns = [
+    path('',views.cargarGatos)
+]
+
+urlpatterns = [
+    path('',views.cargarMas)
+]
+
--- Entrega-3/apps/petSociety/views.py
+++ Entrega-3/apps/petSociety/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def cargarInicio(request):
     return render(request,"index.html")
+
+def cargarPerros(request):
+    return render(request,"perros.html")
+
+def cargarGatos(request):
+    return render(request,"gatos.html")
+
+def cargarMas(request):
+    return render(request,"mas.html")

4112e81c903a98f4ff3c76ba7b44d053b81b221b
fabiibet <fab.betancourt@duocuc.cl>
2023-06-07 23:15:24 -0400

    Atualizacion Vistas

--- Entrega-3/apps/petSociety/urls.py
+++ Entrega-3/apps/petSociety/urls.py
@@ -2,18 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.cargarInicio)
+    path('',views.cargarInicio),
+    path('perros',views.cargarPerros,name = 'perros'),
+    path('gatos',views.cargarGatos,name = 'gatos'),
+    path('mas',views.cargarMas,name = 'mas'),
+    path('inicio',views.cargarInicio,name = 'inicio')
+    
 ]
 
-urlpatterns = [
-    path('',views.cargarPerros)
-]
-
-urlpatterns = [
-    path('',views.cargarGatos)
-]
-
-urlpatterns = [
-    path('',views.cargarMas)
-]
 
--- Entrega-3/apps/petSociety/views.py
+++ Entrega-3/apps/petSociety/views.py
@@ -7,10 +7,10 @@ def cargarInicio(request):
     return render(request,"index.html")
 
 def cargarPerros(request):
-    return render(request,"perros.html")
+     return render(request,"perros.html")
 
 def cargarGatos(request):
-    return render(request,"gatos.html")
+     return render(request,"gatos.html")
 
 def cargarMas(request):
-    return render(request,"mas.html")
+     return render(request,"mas.html")
